2ce718e2f5a5373321da2fda7586ae9a87f32f2a
vvgopal2 <vvgopal2@illinois.edu>
2018-03-15 16:31:07 -0500

    start

---
+++ Lab8/lab8.cdf
@@ -0,0 +1,13 @@
+/* Quartus II 64-Bit Version 15.0.0 Build 145 04/22/2015 SJ Web Edition */
+JedecChain;
+	FileRevision(JESD32A);
+	DefaultMfr(6E);
+
+	P ActionCode(Cfg)
+		Device PartName(EP4CE115F29) Path("C:/Users/Vignesh/Documents/ece385/Lab8/") File("lab8.sof") MfrSpec(OpMask(1));
+
+ChainEnd;
+
+AlteraBegin;
+	ChainType(JTAG);
+AlteraEnd;
---
+++ Lab8/software/usb_kb/obj/default/main.d
@@ -0,0 +1,32 @@
+obj/default/main.o: main.c ../usb_kb_bsp//HAL/inc/io.h \
+ ../usb_kb_bsp//HAL/inc/alt_types.h ../usb_kb_bsp/system.h \
+ ../usb_kb_bsp/linker.h ../usb_kb_bsp//HAL/inc/alt_types.h \
+ ../usb_kb_bsp//HAL/inc/sys/alt_irq.h ../usb_kb_bsp//HAL/inc/nios2.h \
+ ../usb_kb_bsp//HAL/inc/priv/alt_legacy_irq.h io_handler.h cy7c67200.h \
+ usb.h lcp_cmd.h lcp_data.h
+
+../usb_kb_bsp//HAL/inc/io.h:
+
+../usb_kb_bsp//HAL/inc/alt_types.h:
+
+../usb_kb_bsp/system.h:
+
+../usb_kb_bsp/linker.h:
+
+../usb_kb_bsp//HAL/inc/alt_types.h:
+
+../usb_kb_bsp//HAL/inc/sys/alt_irq.h:
+
+../usb_kb_bsp//HAL/inc/nios2.h:
+
+../usb_kb_bsp//HAL/inc/priv/alt_legacy_irq.h:
+
+io_handler.h:
+
+cy7c67200.h:
+
+usb.h:
+
+lcp_cmd.h:
+
+lcp_data.h:
